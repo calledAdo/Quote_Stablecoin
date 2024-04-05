a58592a7d730ff4fed858df3567ed433a678ff26
Greatonic <awosusigabriel2019@gmail.com>
2024-04-05 14:31:22 +0100

    Fixes Commit

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,12 +1,9 @@
-import { AppHeader, Line } from "@components";
+import { AppHeader } from "@components";
 import { useState } from "react";
 import Mint from "./mint";
 import Redeem from "./redeem";
 import Pool from "./pool";
-import { Icon } from "@iconify/react";
-import { openLink } from "../../lib/utils";
-import {motion} from "framer-motion"
-import { METAMASK_LINK, TRUST_WALLET_LINK } from "@constants";
+
 
 export default function AppLanding() {
   const [route, setRoute] = useState<string>("Mint");
