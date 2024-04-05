6e5e4ed1ad21269ebde122e9f1c40a661d4ece16
Greatonic <awosusigabriel2019@gmail.com>
2024-04-05 14:50:04 +0100

    Fix Commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <main className="w-screen h-screen overflow-y-scroll scroll-smooth bg-background">
+      <main className="w-screen h-screen overflow-y-scroll bg-background">
        <Routes>
         <Route path="/" element={ <Landing/>}/>
         <Route path="/app" element={ <AppLanding/>}/>
